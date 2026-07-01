cb3214320370314cad0caff9461dfda922ff942d
teeramateKan <theetitle@gmail.com>
2026-07-01 16:49:56 +0700

    first commit

---
+++ App.tsx
@@ -0,0 +1,49 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import { NewAppScreen } from '@react-native/new-app-screen';
+import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
+import { DatadogProvider } from '@datadog/mobile-react-native';
+import config from './datadogRUM-init';
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <DatadogProvider configuration={config}>
+      <SafeAreaProvider>
+        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+        <AppContent />
+      </SafeAreaProvider>
+    </DatadogProvider>
+  );
+}
+
+function AppContent() {
+  const safeAreaInsets = useSafeAreaInsets();
+
+  return (
+    <View style={styles.container}>
+      <NewAppScreen
+        templateFileName="App.tsx"
+        safeAreaInsets={safeAreaInsets}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
---
+++ datadogRUM-init.js
@@ -0,0 +1,27 @@
+import {
+  DatadogProviderConfiguration,
+  TrackingConsent,
+} from '@datadog/mobile-react-native';
+
+const config = new DatadogProviderConfiguration(
+  'pubf0b42d8cee08deae7e586773b2233193',
+  'dev',
+  TrackingConsent.GRANTED,
+  {
+    rumConfiguration: {
+      applicationId: '9f209013-bf95-4904-ae0b-27b2eb0865ef',
+      trackInteractions: true, // Track user interactions (use the 'accessibilityLabel' prop to give tap actions a custom name)
+      trackResources: true, // Track XHR Resources
+      trackErrors: true, // Track Errors
+      sessionSampleRate: 100, // Optional: Percentage of sampled RUM sessions. Default is 100.
+      resourceTraceSampleRate: 100, // Optional: Percentage of traced network requests. Default is 100.
+      nativeCrashReportEnabled: true, // Optional: Enable Native Crash Reporting
+    },
+    logsConfiguration: {}, // Enable Logs
+    traceConfiguration: {}, // Enable Trace
+  },
+);
+
+config.site = 'US1';
+
+export default config;
